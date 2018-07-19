d63481e3d2c5e2e0210df293a0aa02eb6665140e
Coopermakc <Coopermakc@mail.ru>
2018-07-19 16:25:33 +0300

    comment cable

--- app/assets/javascripts/comments.js.coffee
+++ app/assets/javascripts/comments.js.coffee
@@ -1,6 +1,16 @@
 $ ->
+  commentsList = $('.comments')
   $('.new-comment-link').click (e) ->
     e.preventDefault();
     $(this).hide();
     commentable_id = $(this).data('commentableId')
     $('form#new-comment-' + commentable_id).show()
+  App.cable.subscriptions.create('QuestionsChannel', {
+    connected: ->
+      @perform 'follow'
+    ,
+
+    received: (data) ->
+      console.log(gon.answers)
+      answersList.append gon.answers
+})
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ require 'pry'
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :load_commentable
+  after_action :publish_comments
   def create
     @comment = Comment.new(commentable: @commentable, user_id: current_user.id, comment_body: params[:comment][:comment_body])
     @question = @comment.commentable_type == 'Question' ? @comment.commentable : @comment.commentable.question
@@ -16,5 +17,10 @@ class CommentsController < ApplicationController
 
   def load_commentable
     @commentable = params[:question_id] ? Question.find(params[:question_id]):Answer.find(params[:answer_id])
+  end
+
+  def publish_comments
+    return if @question.errors.any?
+    ActionCable.server.broadcast 'questions', ApplicationController.render(partial: 'comments/question', locals: { comment: @comment })
   end
 end
