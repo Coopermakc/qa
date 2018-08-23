9c927f7a036619cc078b972a83052a34a905bfa7
Coopermakc <Coopermakc@mail.ru>
2018-08-23 17:01:34 +0300

    api 2 test error

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
   def index
     @questions = Question.all
-    respond_with @questions
+    respond_with @questions, root: true
   end
   def show
     respond_with Question.find(params[:id])
