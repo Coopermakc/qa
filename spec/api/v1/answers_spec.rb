e7d94271904832410d0714a198a83f2364f76129
Coopermakc <Coopermakc@mail.ru>
2018-09-05 11:12:39 +0300

    api2 answer spec

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -88,4 +88,44 @@ describe 'Answers api' do
       end
     end
   end
+  describe 'POST /create' do
+    let(:user){ create(:user) }
+    let(:question){ create(:question, user: user) }
+
+    context 'unauthorized' do
+      it 'return 401 status if there is no access token' do
+        post "/api/v1/questions/#{question.id}/answers", params: { format: :json }
+        expect(response.status).to eq 401
+      end
+      it 'return 401 status if access token is invalid' do
+        post "/api/v1/questions/#{question.id}/answers", params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:access_token){ create(:access_token, resource_owner_id: user.id) }
+      context 'with valid attributes' do
+        it 'returns 201 status code' do
+          post "/api/v1/questions/#{question.id}/answers", params: { answer: attributes_for(:answer), format: :json, access_token: access_token.token }
+          expect(response).to be_success
+        end
+        it 'create new answer for user' do
+          expect { (post "/api/v1/questions/#{question.id}/answers", params: { answer: attributes_for(:answer), format: :json, access_token: access_token.token }) }.to change(user.answers, :count).by(1)
+        end
+        it 'create new answer for question' do
+          expect { (post "/api/v1/questions/#{question.id}/answers", params: { answer: attributes_for(:answer), format: :json, access_token: access_token.token }) }.to change(question.answers, :count).by(1)
+        end
+      end
+      context 'with invalid attributes' do
+        it 'returns 422 status code' do
+           post "/api/v1/questions/#{question.id}/answers", params: { answer: attributes_for(:invalid_answer), format: :json, access_token: access_token.token }
+           expect(response.status).to eq 422
+        end
+        it 'does not save invalid answer in db' do
+          expect { (post "/api/v1/questions/#{question.id}/answers", params: { answer: attributes_for(:invalid_answer), format: :json, access_token: access_token.token }) }.to_not change(Answer, :count)
+        end
+      end
+    end
+  end
 end
