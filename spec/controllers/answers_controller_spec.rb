7ce711e3208609516ff038aa59acd5912f2ab3b4
Coopermakc <Coopermakc@mail.ru>
2018-09-25 22:50:37 +0300

    answer controller test

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
       sign_in_user
+      let!(:user) { create(:user) }
       let!(:question) { create(:question, user: @user) }
       let!(:answer) { create(:answer, question: question, user: @user) }
 
